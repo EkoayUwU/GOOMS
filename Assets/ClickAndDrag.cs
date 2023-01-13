83fa29e0eb7904b55fa46c47b6bd37332e0f4829
EEifen <ethangongora24@gmail.com>
2023-01-13 23:50:22 +0100

    cleanage code + props can be lock
    ajout RMB pour lock props

--- Assets/ClickAndDrag.cs
+++ Assets/ClickAndDrag.cs
@@ -12,12 +12,11 @@ public class ClickAndDrag : MonoBehaviour
     //layer Mask Props
     [SerializeField] LayerMask layerMask;
 
-    RaycastHit2D isHitRay;
-
-    [SerializeField] bool isProps;
     Vector2 mousePosition;
-    Rigidbody2D hittedProps;
     Vector2 ref_velocity = Vector2.zero;
+
+    Rigidbody2D hittedProps;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -29,44 +28,40 @@ public class ClickAndDrag : MonoBehaviour
     void Update()
     {
         //Création RayCastHit avec position curseur + direction + taille + layerMask qu'il doit reconnaître
-        isHitRay = Physics2D.Raycast(obstacleRayObject.transform.position, Vector2.right * 5, obstacleRayDistance, layerMask);
-        
-
-        //Si Curseur sur props
-        if (isHitRay.collider)
-        {            
-            //Affichage Raycast
-            //Debug.DrawRay(obstacleRayObject.transform.position, Vector2.right * isHitRay.distance, Color.green);
-            isProps = true;
-        }
-        else
-        {
-            isProps = false;
-        }
-
+        RaycastHit2D isHitRay = Physics2D.Raycast(obstacleRayObject.transform.position, Vector2.right * 5, obstacleRayDistance, layerMask);
+   
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0) && isProps)
+        //Unfreeze props + retirer velocité/gravité pdnt drag
+        if (Input.GetMouseButtonDown(0) && isHitRay)
         {
             hittedProps = isHitRay.transform.gameObject.GetComponent<Rigidbody2D>(); 
             hittedProps= hittedProps.GetComponent<Rigidbody2D>();
 
             hittedProps.velocity = Vector2.zero;
+
+            hittedProps.constraints = RigidbodyConstraints2D.None;
         }
 
-                        
+        //Freeze le props
+        if (Input.GetMouseButtonDown(1) && isHitRay)
+        {
+            hittedProps.constraints = RigidbodyConstraints2D.FreezeAll;
+        }  
+        
+        //
         if (Input.GetMouseButtonUp(0) && hittedProps)
         {
-
             hittedProps = null;
         }       
     }
 
     private void FixedUpdate()
     {
+        //Déplacement props
         if (hittedProps)
         {
-            hittedProps.MovePosition(Vector2.SmoothDamp(hittedProps.transform.position, mousePosition, ref ref_velocity, 0f));
+            hittedProps.MovePosition(Vector2.SmoothDamp(hittedProps.transform.position, mousePosition, ref ref_velocity, 0f));            
             
         }
     }

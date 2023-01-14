352f6d240e2438c4836cbe4ae6a763326e21e715
EEifen <ethangongora24@gmail.com>
2023-01-14 15:20:12 +0100

    Clean code + map
    envie de canner :)

--- Assets/Scripts/ClickAndDrag.cs
+++ Assets/Scripts/ClickAndDrag.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ClickAndDrag : MonoBehaviour
@@ -12,19 +13,19 @@ public class ClickAndDrag : MonoBehaviour
     //layer Mask Props
     [SerializeField] LayerMask layerMask;
 
+
     Vector2 mousePosition;
     Vector2 ref_velocity = Vector2.zero;
 
-    Rigidbody2D hittedProps;
-    
+    private Rigidbody2D hittedProps;
 
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         //Création RayCastHit avec position curseur + direction + taille + layerMask qu'il doit reconnaître
@@ -32,38 +33,38 @@ public class ClickAndDrag : MonoBehaviour
    
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        //Unfreeze props + retirer velocité/gravité pdnt drag
+        //Unfreeze props + retirer velocité/gravité pdnt drag + création parent pour rotation props 
         if (Input.GetMouseButtonDown(0) && isHitRay)
         {
-            hittedProps = isHitRay.transform.gameObject.GetComponent<Rigidbody2D>(); 
-            hittedProps= hittedProps.GetComponent<Rigidbody2D>();
+            hittedProps = isHitRay.transform.gameObject.GetComponent<Rigidbody2D>();
 
             hittedProps.velocity = Vector2.zero;
-
+            hittedProps.gravityScale = 0f;
             hittedProps.constraints = RigidbodyConstraints2D.None;
         }
 
-        //Freeze le props
+
+        //Freeze le props avec RMB 
         if (Input.GetMouseButtonDown(1) && isHitRay)
         {
-            hittedProps.constraints = RigidbodyConstraints2D.FreezeAll;
+            hittedProps.constraints = RigidbodyConstraints2D.FreezeAll;           
         }  
         
-        //
+        //clear la variable qui stock le props séléctioné lors du relachement de LMB
         if (Input.GetMouseButtonUp(0) && hittedProps)
         {
-            hittedProps = null;
-        }       
+            hittedProps.gravityScale = 1f;
+            hittedProps = null;            
+        }          
     }
 
+    
     private void FixedUpdate()
     {
         //Déplacement props
         if (hittedProps)
         {
-            hittedProps.MovePosition(Vector2.SmoothDamp(hittedProps.transform.position, mousePosition, ref ref_velocity, 0f));            
-            
+            hittedProps.MovePosition(Vector2.SmoothDamp(hittedProps.transform.position, mousePosition, ref ref_velocity, 0f));
         }
     }
-
 }
